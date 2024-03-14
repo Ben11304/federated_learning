2afcebbfe6223ad12ea0c237d728b19b571f0b35
Ben11304 <126454553+Ben11304@users.noreply.github.com>
2024-03-14 09:51:59 +0700

    3th commit

--- NonIID/search.py
+++ NonIID/search.py
@@ -1,124 +1,243 @@
-import torch
+import utils, torch, time, os, pickle
+import numpy as np
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader, Dataset
-from torch.autograd.variable import Variable
+from dataloader import dataloader
 
-# Định nghĩa Discriminator
-class Discriminator(nn.Module):
-    def __init__(self, input_dim):
-        super(Discriminator, self).__init__()
-        self.fc1 = nn.Linear(input_dim, 128)
-        self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(128, 64)
-        self.fc3 = nn.Linear(64, 1)
-        self.sigmoid = nn.Sigmoid()
+class generator(nn.Module):
+    # Network Architecture is exactly same as in infoGAN (https://arxiv.org/abs/1606.03657)
+    # Architecture : FC1024_BR-FC7x7x128_BR-(64)4dc2s_BR-(1)4dc2s_S
+    def __init__(self, input_dim=100, output_dim=1, input_size=32, class_num=10):
+        super(generator, self).__init__()
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.input_size = input_size
+        self.class_num = class_num
 
-    def forward(self, x):
-        x = self.fc1(x)
-        x = self.relu(x)
-        x = self.fc2(x)
-        x = self.relu(x)
-        x = self.fc3(x)
-        validity = self.sigmoid(x)
-        return validity
+        self.fc = nn.Sequential(
+            nn.Linear(self.input_dim + self.class_num, 1024),
+            nn.BatchNorm1d(1024),
+            nn.ReLU(),
+            nn.Linear(1024, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+            nn.Linear(512 ,self.output_dim)
+        )
+        utils.initialize_weights(self)
 
-# Định nghĩa Generator
-class Generator(nn.Module):
-    def __init__(self, latent_dim, output_dim):
-        super(Generator, self).__init__()
-        self.fc1 = nn.Linear(latent_dim, 128)
-        self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(128, 64)
-        self.fc3 = nn.Linear(64, output_dim)
-
-    def forward(self, x):
-        x = self.fc1(x)
-        x = self.relu(x)
-        x = self.fc2(x)
-        x = self.relu(x)
-        x = self.fc3(x)
+    def forward(self, input, label):
+        x = torch.cat([input, label], 1)
+        x = self.fc(x)
         return x
 
-# Định nghĩa bộ dữ liệu tùy chỉnh
-class CustomDataset(Dataset):
-    def __init__(self, data, labels):
-        self.data = data
-        self.labels = labels
+class discriminator(nn.Module):
+    # Network Architecture is exactly same as in infoGAN (https://arxiv.org/abs/1606.03657)
+    # Architecture : (64)4c2s-(128)4c2s_BL-FC1024_BL-FC1_S
+    def __init__(self, input_dim=1, output_dim=1, input_size=32, class_num=10):
+        super(discriminator, self).__init__()
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.input_size = input_size
+        self.class_num = class_num
 
-    def __getitem__(self, index):
-        x = self.data[index]
-        y = self.labels[index]
-        return x, y
+        self.conv = nn.Sequential(
+            nn.Conv2d(self.input_dim + self.class_num, 64, 4, 2, 1),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(64, 128, 4, 2, 1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2),
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(self.input_dim+self.calss_num, 1024),
+            nn.BatchNorm1d(1024),
+            nn.LeakyReLU(0.2),
+            nn.Linear(1024, self.output_dim),
+            nn.Sigmoid(),
+        )
+        utils.initialize_weights(self)
 
-    def __len__(self):
-        return len(self.data)
+    def forward(self, input, label):
+        x = torch.cat([input, label], 1)
+        x = self.conv(x)
+        x = self.fc(x)
+        return x
 
-# Cài đặt các tham số
-input_dim = 28
-latent_dim = 10
-output_dim = 28
-num_epochs = 100
-batch_size = 64
-lr = 0.0002
 
-# Khởi tạo discriminator và generator
-discriminator = Discriminator(input_dim)
-generator = Generator(latent_dim, output_dim)
+class CGAN(object):
+    def __init__(self, args):
+        # parameters
+        self.epoch = args.epoch
+        self.batch_size = args.batch_size
+        self.save_dir = args.save_dir
+        self.result_dir = args.result_dir
+        self.dataset = args.dataset
+        self.log_dir = args.log_dir
+        self.gpu_mode = args.gpu_mode
+        self.model_name = args.gan_type
+        self.input_size = args.input_size
+        self.z_dim = 62
+        self.class_num = args.n_class
+        self.sample_num = self.class_num ** 2 
 
-# Định nghĩa hàm loss và optimizer
-adversarial_loss = nn.BCELoss()
-optimizer_D = optim.Adam(discriminator.parameters(), lr=lr)
-optimizer_G = optim.Adam(generator.parameters(), lr=lr)
+        # load dataset
+        self.data_loader = dataloader(self.dataset, self.input_size, self.batch_size)
+        data = self.data_loader.__iter__().__next__()[0]
+        #option
 
-# Chuẩn bị dữ liệu
-data = [...]  # Dữ liệu đầu vào tabular
-labels = [...]  # Nhãn
-dataset = CustomDataset(data, labels)
-dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
-# Huấn luyện mô hình
-for epoch in range(num_epochs):
-    for i, (real_data, labels) in enumerate(dataloader):
-        batch_size = real_data.size(0)
+        # networks init
+        self.G = generator(input_dim=self.z_dim, output_dim=data.shape[1], input_size=self.input_size, class_num=self.class_num)
+        self.D = discriminator(input_dim=data.shape[1], output_dim=1, input_size=self.input_size, class_num=self.class_num)
+        self.G_optimizer = optim.Adam(self.G.parameters(), lr=args.lrG, betas=(args.beta1, args.beta2))
+        self.D_optimizer = optim.Adam(self.D.parameters(), lr=args.lrD, betas=(args.beta1, args.beta2))
 
-        # Chuẩn bị dữ liệu thật và dữ liệu giả
-        real_data = Variable(real_data.float())
-        labels = Variable(labels.float())
-        valid = Variable(torch.ones(batch_size, 1))
-        fake = Variable(torch.zeros(batch_size, 1))
+        if self.gpu_mode:
+            self.G.cuda()
+            self.D.cuda()
+            self.BCE_loss = nn.BCELoss().cuda()
+        else:
+            self.BCE_loss = nn.BCELoss()
 
-        # Huấn luyện Discriminator
-        optimizer_D.zero_grad()
+        print('---------- Networks architecture -------------')
+        utils.print_network(self.G)
+        utils.print_network(self.D)
+        print('-----------------------------------------------')
 
-        # Loss cho dữ liệu thật
-        real_pred = discriminator(real_data)
-        d_loss_real = adversarial_loss(real_pred, valid)
+        # fixed noise & condition
+        self.sample_z_ = torch.zeros((self.sample_num, self.z_dim)) # z_dim tượng trưng cho noise vector, vector nhiều có kích thuóc dài bằng vector onehotencoded^2 của label 
+        for i in range(self.class_num):
+            self.sample_z_[i*self.class_num] = torch.rand(1, self.z_dim)#tạo một vector random có kích thuớc 1, z_dim ( chiều dài của vector nhiễu )
+            for j in range(1, self.class_num):
+                self.sample_z_[i*self.class_num + j] = self.sample_z_[i*self.class_num]
 
-        # Loss cho dữ liệu giả
-        z = Variable(torch.randn(batch_size, latent_dim))
-        gen_labels = Variable(torch.randn(batch_size, output_dim))
-        fake_data = generator(z)
-        fake_pred = discriminator(fake_data)
-        d_loss_fake = adversarial_loss(fake_pred, fake)
+        temp = torch.zeros((self.class_num, 1))
+        for i in range(self.class_num):
+            temp[i, 0] = i
 
-        # Tổng loss cho Discriminator
-        d_loss = d_loss_real + d_loss_fake
-        d_loss.backward()
-        optimizer_D.step()
+        temp_y = torch.zeros((self.sample_num, 1))
+        for i in range(self.class_num):
+            temp_y[i*self.class_num: (i+1)*self.class_num] = temp
 
-        # Huấn luyện Generator
-        optimizer_G.zero_grad()
+        self.sample_y_ = torch.zeros((self.sample_num, self.class_num)).scatter_(1, temp_y.type(torch.LongTensor), 1)
+        if self.gpu_mode:
+            self.sample_z_, self.sample_y_ = self.sample_z_.cuda(), self.sample_y_.cuda()
 
-        # Loss cho Generator
-        z = Variable(torch.randn(batch_size, latent_dim))
-        gen_labels = Variable(torch.randn(batch_size, output_dim))
-        fake_data = generator(z)
-        fake_pred = discriminator(fake_data)
-        g_loss = adversarial_loss(fake_pred, valid)
 
-        g_loss.backward()
-        optimizer_G.step()
+    def train(self):
+        self.train_hist = {}
+        self.train_hist['D_loss'] = []
+        self.train_hist['G_loss'] = []
+        self.train_hist['per_epoch_time'] = []
+        self.train_hist['total_time'] = []
 
-        if (i+1) % 10 == 0:
-            print(f"Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{len(dataloader)}], d_loss: {d_loss.itemLưu ý rằng đoạn mã trên chỉ là một ví dụ cơ bản và cần được tùy chỉnh phù hợp với bài toán của bạn. Bạn cần thay thế dữ liệu và nhãn thật sự vào biến `data` và `labels`, cùng với việc chỉnh sửa các tham số khác như `input_dim`, `latent_dim`, `output_dim`, `num_epochs`, `batch_size`, và `lr` để phù hợp với yêu cầu của bài toán.
+        self.y_real_, self.y_fake_ = torch.ones(self.batch_size, 1), torch.zeros(self.batch_size, 1)
+        if self.gpu_mode:
+            self.y_real_, self.y_fake_ = self.y_real_.cuda(), self.y_fake_.cuda()
+
+        self.D.train()
+        print('training start!!')
+        start_time = time.time()
+        for epoch in range(self.epoch):
+            self.G.train()
+            epoch_start_time = time.time()
+            for iter, (x_, y_) in enumerate(self.data_loader):
+                if iter == self.data_loader.dataset.__len__() // self.batch_size:
+                    break
+
+                z_ = torch.rand((self.batch_size, self.z_dim))
+                y_vec_ = torch.zeros((self.batch_size, self.class_num)).scatter_(1, y_.type(torch.LongTensor).unsqueeze(1), 1)
+                y_fill_ = y_vec_.unsqueeze(2).unsqueeze(3).expand(self.batch_size, self.class_num, self.input_size, self.input_size)
+                if self.gpu_mode:
+                    x_, z_, y_vec_, y_fill_ = x_.cuda(), z_.cuda(), y_vec_.cuda(), y_fill_.cuda()
+
+                # update D network
+                self.D_optimizer.zero_grad()
+
+                D_real = self.D(x_, y_fill_)
+                D_real_loss = self.BCE_loss(D_real, self.y_real_)
+
+                G_ = self.G(z_, y_vec_)
+                D_fake = self.D(G_, y_fill_)
+                D_fake_loss = self.BCE_loss(D_fake, self.y_fake_)
+
+                D_loss = D_real_loss + D_fake_loss
+                self.train_hist['D_loss'].append(D_loss.item())
+
+                D_loss.backward()
+                self.D_optimizer.step()
+
+                # update G network
+                self.G_optimizer.zero_grad()
+
+                G_ = self.G(z_, y_vec_)
+                D_fake = self.D(G_, y_fill_)
+                G_loss = self.BCE_loss(D_fake, self.y_real_)
+                self.train_hist['G_loss'].append(G_loss.item())
+
+                G_loss.backward()
+                self.G_optimizer.step()
+
+                if ((iter + 1) % 100) == 0:
+                    print("Epoch: [%2d] [%4d/%4d] D_loss: %.8f, G_loss: %.8f" %
+                          ((epoch + 1), (iter + 1), self.data_loader.dataset.__len__() // self.batch_size, D_loss.item(), G_loss.item()))
+
+            self.train_hist['per_epoch_time'].append(time.time() - epoch_start_time)
+            with torch.no_grad():
+                self.visualize_results((epoch+1))
+
+        self.train_hist['total_time'].append(time.time() - start_time)
+        print("Avg one epoch time: %.2f, total %d epochs time: %.2f" % (np.mean(self.train_hist['per_epoch_time']),
+              self.epoch, self.train_hist['total_time'][0]))
+        print("Training finish!... save training results")
+
+        self.save()
+        utils.generate_animation(self.result_dir + '/' + self.dataset + '/' + self.model_name + '/' + self.model_name,
+                                 self.epoch)
+        utils.loss_plot(self.train_hist, os.path.join(self.save_dir, self.dataset, self.model_name), self.model_name)
+
+    def visualize_results(self, epoch, fix=True):
+        self.G.eval()
+
+        if not os.path.exists(self.result_dir + '/' + self.dataset + '/' + self.model_name):
+            os.makedirs(self.result_dir + '/' + self.dataset + '/' + self.model_name)
+
+        image_frame_dim = int(np.floor(np.sqrt(self.sample_num)))
+
+        if fix:
+            """ fixed noise """
+            samples = self.G(self.sample_z_, self.sample_y_)
+        else:
+            """ random noise """
+            sample_y_ = torch.zeros(self.batch_size, self.class_num).scatter_(1, torch.randint(0, self.class_num - 1, (self.batch_size, 1)).type(torch.LongTensor), 1)
+            sample_z_ = torch.rand((self.batch_size, self.z_dim))
+            if self.gpu_mode:
+                sample_z_, sample_y_ = sample_z_.cuda(), sample_y_.cuda()
+
+            samples = self.G(sample_z_, sample_y_)
+
+        if self.gpu_mode:
+            samples = samples.cpu().data.numpy().transpose(0, 2, 3, 1)
+        else:
+            samples = samples.data.numpy().transpose(0, 2, 3, 1)
+
+        samples = (samples + 1) / 2
+        utils.save_images(samples[:image_frame_dim * image_frame_dim, :, :, :], [image_frame_dim, image_frame_dim],
+                          self.result_dir + '/' + self.dataset + '/' + self.model_name + '/' + self.model_name + '_epoch%03d' % epoch + '.png')
+
+    def save(self):
+        save_dir = os.path.join(self.save_dir, self.dataset, self.model_name)
+
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+
+        torch.save(self.G.state_dict(), os.path.join(save_dir, self.model_name + '_G.pkl'))
+        torch.save(self.D.state_dict(), os.path.join(save_dir, self.model_name + '_D.pkl'))
+
+        with open(os.path.join(save_dir, self.model_name + '_history.pkl'), 'wb') as f:
+            pickle.dump(self.train_hist, f)
+
+    def load(self):
+        save_dir = os.path.join(self.save_dir, self.dataset, self.model_name)
+
+        self.G.load_state_dict(torch.load(os.path.join(save_dir, self.model_name + '_G.pkl')))
+        self.D.load_state_dict(torch.load(os.path.join(save_dir, self.model_name + '_D.pkl')))
